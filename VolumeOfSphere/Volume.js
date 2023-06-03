7de4960bc51faa38226d72b7db95a25e69184be2
Vishnu Mohan P <8509693+findviz@users.noreply.github.com>
2023-06-03 15:08:45 +0530

    Add files via upload

---
+++ VolumeOfSphere/Volume.js
@@ -0,0 +1,6 @@
+function volumeOfSphere() {
+  let radius = document.getElementById("radius").value;
+  let volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+  volume = volume.toFixed(4);
+  document.getElementById("volume").value = volume;
+}
